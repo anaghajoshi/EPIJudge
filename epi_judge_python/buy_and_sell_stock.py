c8d1eb6532f3c2711cd6905b6cc3a9f7fd9acda1
Anagha Joshi <anagha.elex@gmail.com>
2018-07-14 13:08:43 -0700

    added buy_and_sell_stock solution

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -3,7 +3,13 @@ from test_framework import generic_test
 
 def buy_and_sell_stock_once(prices):
     # TODO - you fill in here.
-    return 0.0
+    max_profit = 0.0
+    for i in range(len(prices)-1):
+        max_price_future = max(prices[i+1:])
+        profit_poss = max_price_future - prices[i]
+        if profit_poss > max_profit:
+            max_profit = profit_poss
+    return max_profit
 
 
 if __name__ == '__main__':
